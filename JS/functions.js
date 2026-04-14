f03b3f7332e4474ab8dca4a860934f10b8f61aba
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-04-14 17:21:04 +0530

    functions and console

---
+++ JS/functions.js
@@ -0,0 +1,19 @@
+var a = 23;
+var b = 24;
+var c = 234;
+var d = 343;
+var e = 353;
+
+function evenOdd(x) {
+  if (x % 2 == 0) {
+    console.log(x, " is even");
+  } else {
+    console.log(x, " is odd");
+  }
+}
+
+evenOdd(a)
+evenOdd(b)
+evenOdd(c)
+evenOdd(d)
+evenOdd(e)
