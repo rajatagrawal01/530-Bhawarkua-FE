6c74f3fb78e02d67700a3d5bac3133fa72dd01e2
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-04-16 17:15:12 +0530

    functions

--- JS/functions.js
+++ JS/functions.js
@@ -4,16 +4,49 @@ var c = 234;
 var d = 343;
 var e = 353;
 
-function evenOdd(x) {
-  if (x % 2 == 0) {
-    console.log(x, " is even");
-  } else {
-    console.log(x, " is odd");
-  }
+// function evenOdd(x) {
+//   if (x % 2 == 0) {
+//     console.log(x, " is even");
+//   } else {
+//     console.log(x, " is odd");
+//   }
+// }
+
+// evenOdd(a)
+// evenOdd(b)
+// evenOdd(c)
+// evenOdd(d)
+// evenOdd(e)
+
+// Return type function
+// function add(a,b){
+//   console.log("Return type function");
+//   return a+b;
+// }
+
+//Non-Return type function
+
+// function sum(a,b){
+//   console.log("Non-Return type function");
+//   console.log(a+b);
+// }
+
+
+// var x=34
+// sum(2,3)
+// // var y=add(2,3)
+
+// console.log(add(2,3)+5);
+// console.log(console.log());
+
+
+
+function greeting(name="Guest") {
+  console.log("Hello and welcome :",name);
 }
 
-evenOdd(a)
-evenOdd(b)
-evenOdd(c)
-evenOdd(d)
-evenOdd(e)
+greeting()
+greeting("Tanisha")
+greeting("Bhavesh")
+greeting("Ravi")
+
