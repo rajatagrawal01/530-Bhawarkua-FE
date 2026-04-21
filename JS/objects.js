74550c740ba2fa14ad657cba61cd4f980aa319b0
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-04-21 17:14:26 +0530

    object

---
+++ JS/objects.js
@@ -0,0 +1,70 @@
+var stuData = ["Rahul", 24, 9825678668, "81,Bhawarkua Indore"];
+var marks = {
+  name: "Bhavesh",
+  phy: 56,
+  math: 34,
+  chem: 70,
+  Eng: 65,
+  Hin: 50,
+};
+
+console.log(Object.keys(marks));
+console.log(Object.values(marks));
+console.log(Object.entries(marks));
+
+
+
+
+
+// console.log(stuData[0]);
+// console.log(marks.phy,marks.math,marks.Eng,marks.Hin,marks.chem);
+
+var be2ndyear = [
+  {
+    name: "Bhavesh",
+    phy: 56,
+    math: 45,
+    chem: 70,
+    Eng: 70,
+    Hin: 50,
+  },
+  {
+    name: "Ravi",
+    phy: 56,
+    math: 34,
+    chem: 65,
+    Eng: 90,
+    Hin: 50,
+  },
+  {
+    name: "Tanisha",
+    phy: 36,
+    math: 34,
+    chem: 30,
+    Eng: 35,
+    Hin: 40,
+  },
+  {
+    name: "Rajat",
+    marks: {
+      phy: 36,
+      math: 34,
+      chem: 30,
+      Eng: 35,
+      Hin: 40,
+    },
+    pass:true
+},
+    {
+    name: "Kanchan",
+    courses:["Java","React","SQL"]
+  }
+];
+
+// console.log(be2ndyear[2].name);
+
+// console.log(be2ndyear[3].marks.phy);
+
+// console.log(be2ndyear[4].courses[2]);
+
+
