8f988e3ad86985853683ed04d66c1484ab40703e
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-05-07 17:27:13 +0530

    events

--- AdvJs/events.js
+++ AdvJs/events.js
@@ -1,18 +1,49 @@
-var inpt=document.getElementById("stuName")
-var btn=document.getElementById("btn")
+var inpt=document.getElementById("data")
 
-inpt.onkeyup=function(e){
-    // console.log(inpt.value);
-    console.log("Input event",e);
-    //console.log("e.target",e.target); // event trigger element
-    // console.log("e value",e.target.value);
+inpt.onkeyup=function (e){
+  // console.log("Event: ",e);
+  console.log(inpt.value);
+  var msg=document.getElementById("message")
+  msg.innerText=inpt.value
+  
 }
 
-// btn.onkeyup=function (e) {
-//     console.log("Hello sakshi");
-//     console.log(e);
+var btn=document.getElementById("btn")
+btn.onclick=function (e) {
+    console.log(e);
+    // alert("Welcome")
+}
 
-// }
+
+var pwd=document.getElementById("pwd")
+var warn=document.getElementById("warning")
+
+pwd.onkeyup=function (){
+  var password=pwd.value;
+console.log(password.length);
+
+  if(password.length<6){
+     warn.innerText="Enter Password more than 6"
+  }else{
+     warn.innerText=""
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // inpt.addEventListener("keyup",
 //     function(){
@@ -20,18 +51,18 @@ inpt.onkeyup=function(e){
 //     }
 // )
 
-var myform = document.getElementById("demo");
-myform.addEventListener("submit", function (e) {
-  e.preventDefault();
-  var stuName = document.getElementById("stuName").value;
-  var agree = document.getElementById("agree");
-  if (!agree.checked) {
-    alert("You must accept terms");
-  }
+// var myform = document.getElementById("demo");
+// myform.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   var stuName = document.getElementById("stuName").value;
+//   var agree = document.getElementById("agree");
+//   if (!agree.checked) {
+//     alert("You must accept terms");
+//   }
 
-  if (stuName.length < 5) {
-    console.log("Enter greater than 5");
-  } else {
-    console.log("Great");
-  }
-});
+//   if (stuName.length < 5) {
+//     console.log("Enter greater than 5");
+//   } else {
+//     console.log("Great");
+//   }
+// });
