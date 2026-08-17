9e2e8cc2cbd9fefbad2188663fd580cca71d1f38
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-08-17 15:00:22 +0530

    useState Array

--- React/myapp/src/App.js
+++ React/myapp/src/App.js
@@ -5,11 +5,12 @@ import StateHook from './Components/StateHook';
 import { useState } from 'react';
 
 function App() {
-  const [visibility, setVisibility] = useState(true);
+  // const [visibility, setVisibility] = useState(true);
   return (
     <>
-      {visibility && <StateHook />}
-      <button onClick={() => setVisibility(!visibility)}>Click Here</button>
+      {/* {visibility && <StateHook />}
+      <button onClick={() => setVisibility(!visibility)}>Click Here</button> */}
+      <StateHook/>
     </>
   );
 }
--- React/myapp/src/Components/StateHook.js
+++ React/myapp/src/Components/StateHook.js
@@ -1,28 +1,42 @@
 import React, { useState } from 'react'
 
 export default function StateHook() {
-    const [count, setCount] = useState(0);
-    const [name,setName] = useState("");
-    const [age,setAge] = useState(0);
+    // const [count, setCount] = useState(0);
+    // const [name,setName] = useState("");
+    // const [age,setAge] = useState(0);
+    const [names,setNames] = useState(["Rahul", "Rajesh","Rohan","Raj"])
     
 
-    function increase(){
-        setCount(count+1);
-    }
+    // function increase(){
+    //     setCount(count+1);
+    // }
     
-    function decrease(){
-        setCount(count-1);
+    // function decrease(){
+    //     setCount(count-1);
+    // }
+
+    function addName(){
+        setNames([...names,"Rajat"]);
     }
     return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={increase}>Increase</button>
-            <button onClick={decrease}>Decrease</button>
+        // <div>
+        //     <h1>{count}</h1>
+        //     <button onClick={increase}>Increase</button>
+        //     <button onClick={decrease}>Decrease</button>
 
-            <h1>Hi, Your name is {name} and Your Age is {age}</h1>
-            <input onChange={(e)=>setName(e.target.value)} placeholder='Enter Your Name' />
-            <input onChange={(e)=>setAge(e.target.value)} placeholder='Enter Your Age'/>
+        //     <h1>Hi, Your name is {name} and Your Age is {age}</h1>
+        //     <input onChange={(e)=>setName(e.target.value)} placeholder='Enter Your Name' />
+        //     <input onChange={(e)=>setAge(e.target.value)} placeholder='Enter Your Age'/>
             
-        </div>
+        // </div>
+        <>
+            <div>
+                {names.map((name, index) => (
+                    <p key={index}>{name}</p>
+                ))}
+            </div>
+            <input type='text' placeholder='Enter Your Name' onChange={(e)=>setNames(...names,e.target.value)}/>
+            {/* <button onClick={addName}>Click Here</button> */}
+        </>
     )
 }
